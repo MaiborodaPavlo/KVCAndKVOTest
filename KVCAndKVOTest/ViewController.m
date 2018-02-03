6eb8ae120ce0d9b0cd986af31ef0ac299f3c6aba
Pavel Mayboroda <Successbloger1@gmail.com>
2018-02-03 17:30:23 +0200

    Initial Commit

---
+++ KVCAndKVOTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVCAndKVOTest
+//
+//  Created by Pavel on 03.02.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
